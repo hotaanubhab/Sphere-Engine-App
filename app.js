9edec0671f2d6c3121875621e4253fbf43613a72
Änubhab <hota000anubhab@gmail.com>
2021-09-01 01:48:32 +0530

    testcase add works

--- app.js
+++ app.js
@@ -45,13 +45,65 @@ app.get('/submit',(req,res)=>{
     res.render('submit');
 })
 
+app.get('/details',(req,res)=>{
+    res.render('details');
+})
+
+app.get('/testcase',(req,res)=>{
+    res.render('testcase');
+})
+app.post('/testcase',(req,res)=>{
+    // define request parameters
+var problemId = req.body.id;
+var testcaseData = {
+    input: req.body.input,
+    output: req.body.output,
+    timelimit: req.body.timeLimit,
+    judgeId: 1
+};
+
+// send request
+request({
+    
+    url: 'https://' + endpoint + '/api/v4/problems/' + problemId +  '/testcases?access_token=' + accessToken,
+    method: 'POST',
+    form: testcaseData
+}, function (error, response, body) {
+    
+    if (error) {
+        console.log('Connection problem');
+    }
+    
+    // process response
+    if (response) {
+        if (response.statusCode === 201) {
+            console.log(JSON.parse(response.body)); // testcase data in JSON
+        }
+        else {
+            if (response.statusCode === 401) {
+                console.log('Invalid access token');
+            } else if (response.statusCode === 403) {
+                console.log('Access denied');
+            } else if (response.statusCode === 404) {
+                console.log('Problem does not exist');
+            } else if (response.statusCode === 400) {
+                var body = JSON.parse(response.body);
+                console.log('Error code: ' + body.error_code + ', details available in the message: ' + body.message)
+            }
+        }
+    }
+});
+    res.redirect('/testcase');
+})
+
 app.get('/create',(req,res)=>{
     res.render('create');
 })
 app.post('/create',(req,res)=>{
     var problemData = {
         name: req.body.name,
-        masterjudgeId: req.body.masterjudgeId
+        masterjudgeId: req.body.masterjudgeId,
+        body: req.body.body
     };
     
     // send request
