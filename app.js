69e2459fa2f7a89dc2c78a2a79eeed7268e53c10
Änubhab <hota000anubhab@gmail.com>
2021-09-01 06:54:08 +0530

    Status of submission fetching

--- app.js
+++ app.js
@@ -105,7 +105,69 @@ request({
 });
 })
 app.post('/submit',(req,res)=>{
+    
+// define request parameters
+var submissionData = req.body;
 
+// send request
+request({
+    url: 'https://' + endpoint + '/api/v4/submissions?access_token=' + accessToken,
+    method: 'POST',
+    form: submissionData
+}, function (error, response, body) {
+    
+    if (error) {
+        console.log('Connection problem');
+    }
+    
+    // process response
+    if (response) {
+        if (response.statusCode === 201) {
+            console.log(JSON.parse(response.body)); // submission data in JSON
+            // define request parameters
+            var submissionId = JSON.parse(response.body).id;
+
+            // send request
+            setTimeout(function(){ 
+                request({
+                    url: 'https://' + endpoint + '/api/v4/submissions/' + submissionId + '?access_token=' + accessToken,
+                    method: 'GET'
+                }, function (error, response, body) {
+                    
+                    if (error) {
+                        console.log('Connection problem');
+                    }
+                    
+                    // process response
+                    if (response) {
+                        if (response.statusCode === 200) {
+                            //console.log(JSON.parse(response.body)); // submission data in JSON
+                            res.render('status',{s:JSON.parse(response.body)})
+                        } else {
+                            if (response.statusCode === 401) {
+                                console.log('Invalid access token');
+                            } else if (response.statusCode === 403) {
+                                console.log('Access denied');
+                            } else if (response.statusCode === 404) {
+                                console.log('Submision not found');
+                            }
+                        }
+                    }
+                });
+             }, 6000);
+            
+        } else {
+            if (response.statusCode === 401) {
+                console.log('Invalid access token');
+            } else if (response.statusCode === 402) {
+                console.log('Unable to create submission');
+            } else if (response.statusCode === 400) {
+                var body = JSON.parse(response.body);
+                console.log('Error code: ' + body.error_code + ', details available in the message: ' + body.message)
+            }
+        }
+    }
+});
 })
 
 
